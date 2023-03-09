1aab6f3efc2f7ca1b6a7d68d0a9e118c3af02e99
Zahari A . Romanov <109317065+zahariromanov@users.noreply.github.com>
2023-03-09 11:28:06 -0500

    spell check

--- Recommender Systems/book_recommender/recommender.py
+++ Recommender Systems/book_recommender/recommender.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from surprise import Reader
 
-book_ratings = pd.read_csv(r'Tensorflow-Projects\Reccomender Engines\book_reccomender\ratedata.csv')
+book_ratings = pd.read_csv(r'Tensorflow-Projects\Recommender Systems\book_recommender\ratedata.csv')
 print(book_ratings.head())
 
 #1. Print dataset size and examine column data types
